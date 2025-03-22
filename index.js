affc4613a680a28a3f7ca45d324cec5847320217
swimpyblaq <onuchukwuchima@gmail.com>
2025-03-22 22:29:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the containerized Node.js app!');
+  res.send('God is the greatest!!!');
 });
 
 app.listen(PORT, () => {
